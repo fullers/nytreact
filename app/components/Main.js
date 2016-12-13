7a808b429470110e2424fb2727626fa05e242dde
Shaun Fuller <fullers@email.unc.edu>
2016-12-12 20:12:12 -0500

    updated main.js

---
+++ app/components/Main.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+// Import sub-components
+import Searched from "./children/Searched";
+import Saved from "./children/Saved";
+
+// Helper Function
+import helpers from "./utils/helpers";
+
+class Main extends React.Component { 
+
+	
+
+}
+
+// Export the componen back for use in other files
+export default Main;
